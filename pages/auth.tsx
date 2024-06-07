7588e977b7b8198d62e7cac10da9cb6e166cc614
Maria Leitão <malufln.99@gmail.com>
2024-06-07 18:58:43 -0300

    Initial implementation of Sign In input

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -9,7 +9,14 @@ const Auth = () => {
                 </nav>
                 <div>
                     <div className="flex justify-center">
-
+                        <div className="bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+                            <h2 className="text-white text-4xl mb-8 font-semibold">
+                                Sign In
+                            </h2>
+                            <div className="flex flex-col gap-4">
+                                <Input />
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
