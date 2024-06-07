ee723c9d1d6e03661fdad1c5f049334350760728
Maria Leitão <malufln.99@gmail.com>
2024-06-07 18:51:02 -0300

    Add initial CSS for logo and background image on login screen

---
+++ pages/auth.tsx
@@ -0,0 +1,20 @@
+import Input from "@/components/input";
+
+const Auth = () => {
+    return (
+        <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                <nav className="px-12 py-5">
+                    <img src="/images/logo.png" alt="Logo" className="h-20 ml-auto mr-auto lg:ml-0 lg:mr-0 lg:h-14"></img>
+                </nav>
+                <div>
+                    <div className="flex justify-center">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Auth;
