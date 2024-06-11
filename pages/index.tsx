b402dd4cc81d97e034b78e09440c986dc6ac2ddc
Maria Leitão <malufln.99@gmail.com>
2024-06-11 13:55:29 -0300

    Navbar component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import { NextPageContext } from "next";
 import { getSession, signOut } from "next-auth/react";
@@ -24,9 +25,7 @@ export default function Home() {
 
   return (
     <>
-      <h1>Martialflix</h1>
-      <p className="text-white">Logged in as {user?.name} </p>
-      <button className="h-10 w-full bg-white" onClick={() => signOut()}>Logout</button>
+      <Navbar />
     </>
   );
 }
