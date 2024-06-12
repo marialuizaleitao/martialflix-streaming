073f49413b1b3f38b9a2f5d47d92aca51a248ad8
Maria Leitão <malufln.99@gmail.com>
2024-06-12 16:27:01 -0300

    Initial implementation of My List of favorites

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BsFillPlayFill } from "react-icons/bs";
+import FavoriteButton from "./FavoriteButton";
 
 interface MovieCardProps {
   data: Record<string, any>;
@@ -18,8 +19,9 @@ const MovieCard: React.FC<MovieCardProps> = ({
         <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
           <div className="flex flex-row items-center gap-3">
             <div className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300" onClick={() => { }}>
-              <BsFillPlayFill size={30}/>
+              <BsFillPlayFill size={30} />
             </div>
+            <FavoriteButton movieId={data?.id} />
           </div>
           <p className="text-green-400 font-semibold mt-4">New <span className="text-white">2023</span></p>
           <div className="flex flex-row mt-4 gap-2 items-center">
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -10,71 +10,69 @@ import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID || "",
-      clientSecret: process.env.GITHUB_SECRET || "",
-    }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
-    }),
-    Credentials({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "text",
-        },
-        password: {
-          label: "Password",
-          type: "password",
-        },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email) {
-          throw new Error("Please enter a valid email.");
-        }
-        if (!credentials?.password) {
-          throw new Error(
-            "Your password must contain between 4 and 60 characters."
-          );
-        }
+ providers: [
+  GithubProvider({
+   clientId: process.env.GITHUB_ID || "",
+   clientSecret: process.env.GITHUB_SECRET || "",
+  }),
+  GoogleProvider({
+   clientId: process.env.GOOGLE_CLIENT_ID || "",
+   clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+  }),
+  Credentials({
+   id: "credentials",
+   name: "Credentials",
+   credentials: {
+    email: {
+     label: "Email",
+     type: "text",
+    },
+    password: {
+     label: "Password",
+     type: "password",
+    },
+   },
+   async authorize(credentials) {
+    if (!credentials?.email) {
+     throw new Error("Please enter a valid email.");
+    }
+    if (!credentials?.password) {
+     throw new Error("Your password must contain between 4 and 60 characters.");
+    }
 
-        const user = await prismadb.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
+    const user = await prismadb.user.findUnique({
+     where: {
+      email: credentials.email,
+     },
+    });
 
-        if (!user || !user.hashedPassword) {
-          throw new Error("An account with this Email does not exist.");
-        }
+    if (!user || !user.hashedPassword) {
+     throw new Error("An account with this Email does not exist.");
+    }
 
-        const isCorrectPasword = await compare(
-          credentials.password,
-          user.hashedPassword
-        );
+    const isCorrectPasword = await compare(
+     credentials.password,
+     user.hashedPassword
+    );
 
-        if (!isCorrectPasword) {
-          throw new Error(`Incorrect password for ${credentials.email}`);
-        }
+    if (!isCorrectPasword) {
+     throw new Error(`Incorrect password for ${credentials.email}`);
+    }
 
-        return user;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/auth",
-  },
-  debug: process.env.NODE_ENV === "development",
-  adapter: PrismaAdapter(prismadb),
-  session: {
-    strategy: "jwt",
-  },
-  jwt: {
-    secret: process.env.NEXTAUTH_JWT_SECRET,
-  },
-  secret: process.env.NEXTAUTH_SECRET,
+    return user;
+   },
+  }),
+ ],
+ pages: {
+  signIn: "/auth",
+ },
+ debug: process.env.NODE_ENV === "development",
+ adapter: PrismaAdapter(prismadb),
+ session: {
+  strategy: "jwt",
+ },
+ jwt: {
+  secret: process.env.NEXTAUTH_JWT_SECRET,
+ },
+ secret: process.env.NEXTAUTH_SECRET,
 });
