6760cb508166079bbc34d06791450d30adf80297
Maria Leitão <malufln.99@gmail.com>
2024-06-12 14:04:38 -0300

    Movie List and Movie Card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,9 @@
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
-import useCurrentUser from "@/hooks/useCurrentUser";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
-import { getSession, signOut } from "next-auth/react";
-import { SiBillboard } from "react-icons/si";
+import { getSession } from "next-auth/react";
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -23,12 +23,15 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
-  const { data: user } = useCurrentUser();
+  const { data: movies = [] } = useMovieList();
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies}/>
+      </div>
     </>
   );
 }
